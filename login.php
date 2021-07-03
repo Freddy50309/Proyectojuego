6041f3d50d7ee781f6d0eeb1e468b786fb61d855
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 18:42:49 -0500

    Update login.php
    Rediseño del Fondo

--- login.php
+++ login.php
@@ -1,42 +1,49 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_login.css">
+  <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_login.css">
 </head>
- 
+
+
 <body>
- 
+
 <div class="container">
 
- 
+
     <div class="login-wrap">
         <div class="login-html">
-          
+
           <div class="login-form">
             <div class="sign-in-htm">
+
+            <form action="logica/loguear.php" method="POST">
               <div class="group">
-                  
-                 <br></br> 
+
                  <br></br>
                  <br></br>
                  <br></br>
-                  
-                <label for="user" class="label">nombre</label>
-                <input id="user" type="text" class="input">
+                 <br></br>
+
+             <b>   <label for="user" class="label">Nombre</label>
+                <input id="user" style="color:#050505" name="usuario" type="text" class="input">
               </div>
               <div class="group">
                 <label for="pass" class="label">Apellido </label>
-                <input id="pass" type="password" class="input" data-type="password">
+                <input id="pass" style="color:#050505" name="clave" type="password" class="input" data-type="password">
+
               </div>
-             
-                
+
+              </b>
                 <div class="modal-footer">
-                  <a href="#" class="boton azul">Aceptar</a>
-                  <a href="#" class="boton rojo">Cancelar</a>
+
+                  <button id="envio" class="boton azul">INICIAR</button>
+                  <a href="inicio.php" class="boton rojo">CANCELAR</a>
+
                 </div>
+                </form>
             </div>
           </div>
         </div>
